545f6e5b6e1235fbeae4d27cdb6c5d51394baf91
Luis Miguel <luismiguel.ramos.escola@gmail.com>
2024-02-29 11:30:07 -0300

    Primeiro Commit / Push

---
+++ AftoRoboric/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace AftoRoboric;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ AftoRoboric/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace AftoRoboric;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
