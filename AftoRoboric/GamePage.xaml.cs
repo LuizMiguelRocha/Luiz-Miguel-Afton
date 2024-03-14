a17bc5bade1cebd75452c5bdce1c21b388fa7103
Luiz Miguel <luizmiguel.ramos.escola@gmail.com>
2024-03-14 11:55:01 -0300

    arumar o inicio

--- AftoRoboric/GamePage.xaml.cs
+++ AftoRoboric/GamePage.xaml.cs
@@ -18,17 +18,46 @@ public partial class GamePage : ContentPage
     Id= 0,
     Texto = "Você acorda de manhã maio cansado poos o caso que você esta tentando resolver não avança",
     TemResposta=false,
-   });     
+   });
+
+   historia.Add(new HistoryStep()
+    {
+        Id=1,
+        Texto="Você na sua geladeira e você na sabe se você toma um café normalmente como todo dia ou se você toma um copo de suco",
+        TemResposta=true,
+        TextoDaResposta01="Tomar café",
+        TextoDaResposta02="Tomar um copo de suco",
+        TextoDaResposta03="",
+        IdLevelResposta01=2,
+        IdLevelResposta02=3,
+        IdLevelResposta03=1,
     }
+    );
+
+    historia.Add(new HistoryStep()
+    {
+        Id=2,
+        Texto="Você pega uma xicara e toma um pouco de café com bolacha",
+        TemResposta=false,
+    });
+
+    historia.Add(new HistoryStep()
+    {
+        Id=3,
+        Texto="Você pega um copo de suco e come um pouco de bolacha",
+        TemResposta=false,
+    });     
+    }
+
 
    void PreencherPagina()
    {
     labelTexto.Text = HistoryStepAtual.Texto;
 
     if (HistoryStepAtual.HeroIsDead)
-        frameGamerOver.IsVisible = true;
+        frameGameOver.IsVisible = true;
     else
-        frameGamerOver.IsVisible = false;
+        frameGameOver.IsVisible = false;
 
     if (HistoryStepAtual.TemResposta)
     {
@@ -41,6 +70,15 @@ public partial class GamePage : ContentPage
         Resposta3.Text = HistoryStepAtual.TextoDaResposta03;
     }
       
-   } 
+    else
+    {
+        BotaoProximo.IsVisible = true;
+        Resposta1.IsVisible = false;
+        Resposta2.IsVisible = false;
+        Resposta3.IsVisible = false;
+    }
+   }
+
+
 }
     
