0d683e6b7734f04fe0071edca858169d487bb27e
Luiz Miguel <luizmiguel.ramos.escola@gmail.com>
2024-03-18 09:01:48 -0300

    a historia esta avançandp

--- AftoRoboric/GamePage.xaml.cs
+++ AftoRoboric/GamePage.xaml.cs
@@ -2,83 +2,18 @@ namespace AftoRoboric;
 
 public partial class GamePage : ContentPage
 {
-  
-
-  List<HistoryStep> historia = new List<HistoryStep>();
-  HistoryStep HistoryStepAtual;
-
-  
+    int count = 0;
 
 	public GamePage()
     {
-        InitializeComponent();
-
-     historia.Add(new HistoryStep()
-   {
-    Id= 0,
-    Texto = "Você acorda de manhã maio cansado poos o caso que você esta tentando resolver não avança",
-    TemResposta=false,
-   });
-
-   historia.Add(new HistoryStep()
-    {
-        Id=1,
-        Texto="Você na sua geladeira e você na sabe se você toma um café normalmente como todo dia ou se você toma um copo de suco",
-        TemResposta=true,
-        TextoDaResposta01="Tomar café",
-        TextoDaResposta02="Tomar um copo de suco",
-        TextoDaResposta03="",
-        IdLevelResposta01=2,
-        IdLevelResposta02=3,
-        IdLevelResposta03=1,
-    }
-    );
-
-    historia.Add(new HistoryStep()
-    {
-        Id=2,
-        Texto="Você pega uma xicara e toma um pouco de café com bolacha",
-        TemResposta=false,
-    });
-
-    historia.Add(new HistoryStep()
-    {
-        Id=3,
-        Texto="Você pega um copo de suco e come um pouco de bolacha",
-        TemResposta=false,
-    });     
+        InitializeComponent();     
     }
 
 
-   void PreencherPagina()
-   {
-    labelTexto.Text = HistoryStepAtual.Texto;
-
-    if (HistoryStepAtual.HeroIsDead)
-        frameGameOver.IsVisible = true;
-    else
-        frameGameOver.IsVisible = false;
-
-    if (HistoryStepAtual.TemResposta)
-    {
-        BotaoProximo.IsVisible = false;
-        Resposta1.IsVisible = true;
-        Resposta2.IsVisible = true;
-        Resposta3.IsVisible = true;
-        Resposta1.Text = HistoryStepAtual.TextoDaResposta01;
-        Resposta2.Text = HistoryStepAtual.TextoDaResposta02;
-        Resposta3.Text = HistoryStepAtual.TextoDaResposta03;
-    }
-      
-    else
-    {
-        BotaoProximo.IsVisible = true;
-        Resposta1.IsVisible = false;
-        Resposta2.IsVisible = false;
-        Resposta3.IsVisible = false;
-    }
-   }
-
-
+    	 void BotaoGamePage1(object sender,EventArgs args)
+	{
+		Application.Current.MainPage = new GamePage1();
+	}
+    
 }
     
---
+++ AftoRoboric/GamePage1.xaml.cs
@@ -0,0 +1,11 @@
+namespace AftoRoboric;
+
+public partial class GamePage1 : ContentPage
+{
+    int count = 0;
+
+	public GamePage1()
+    {
+        InitializeComponent();     
+    }
+}    
